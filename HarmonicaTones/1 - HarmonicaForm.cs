a17bf21520d547ff1605784f78a671eff555f2b7
jplsanchez <jpaulo_sanchez@hotmail.com>
2021-08-07 15:42:54 -0300

    Remoção de imports não utilizados

--- HarmonicaTones/1 - HarmonicaForm.cs
+++ HarmonicaTones/1 - HarmonicaForm.cs
@@ -1,14 +1,4 @@
-﻿using HarmonicaTones.Properties;
-using HarmonicaTones.res;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace HarmonicaTones
